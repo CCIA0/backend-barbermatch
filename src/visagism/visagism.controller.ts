2709b306619d2d84cdf519df62e883b158a3c00b
CCIA0 <cciao.oficial@gmail.com>
2025-09-16 18:38:25 -0300

    arreglos en controller de visagism

--- src/visagism/visagism.controller.ts
+++ src/visagism/visagism.controller.ts
@@ -1,5 +1,12 @@
-import { Controller, Post, Body, ValidationPipe, BadRequestException, InternalServerErrorException } from '@nestjs/common';
-import { VisagismService } from './services/visagismo.service';
+import { 
+  Controller, 
+  Post, 
+  Body, 
+  ValidationPipe, 
+  BadRequestException, 
+  InternalServerErrorException 
+} from '@nestjs/common';
+import { VisagismService } from './visagism.service';
 import { AnalyzeImageDto } from './dto/analyze-image.dto';
 
 @Controller('visagism')
@@ -16,16 +23,5 @@ export class VisagismController {
       }
       throw new InternalServerErrorException('Error analyzing face image');
     }
-  }
-}troller, Post, Body } from '@nestjs/common';
-import { VisagismService } from './visagism.service';
-
-@Controller('visagism')
-export class VisagismController {
-  constructor(private readonly visagismService: VisagismService) {}
-
-  @Post('analyze')
-  async analyze(@Body() body: { image: any }) {
-    return this.visagismService.analyzeImage(body.image);
   }
 }
